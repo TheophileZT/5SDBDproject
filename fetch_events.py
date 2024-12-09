fd91d6771b7f176d876e2b98c701bcdac355d241
kelian.sebaici <kelian.sebaici@teamber.fr>
2024-12-09 16:22:55 +0100

    db connection?

--- fetch_events.py
+++ fetch_events.py
@@ -1,11 +1,26 @@
 import requests
-import csv
 import sys
-import os
 import json
+from pymongo import MongoClient
 
-CSV_FILE = "evenements_culturels.csv"
 CONFIG_FILE = 'config.json'
+
+uri="mongodb+srv://theozt25:MGZ7Osyw7gMrGU4O@project.5ulkz.mongodb.net/"
+dbName="myDatabase"
+collectionName="Events"
+
+def save_to_mongodb(data, mongo_uri, database_name, collection_name):
+    try:
+        client = MongoClient(mongo_uri)
+        db = client[database_name]
+        collection = db[collection_name]
+        collection.insert_many(data)
+
+        print(f"Data saved successfully to MongoDB in database '{database_name}', collection '{collection_name}'.\n")
+    except Exception as e:
+        print(f"Error while saving to MongoDB: {e}")
+    finally:
+        client.close()
 
 def usage(script_name, message):
     """
@@ -33,14 +48,13 @@ def read_arguments():
     city_name = sys.argv[1]
     return city_name
 
-
 def load_config():
     """
     Charge le fichier de configuration JSON.
     """
     with open(CONFIG_FILE, 'r', encoding='utf-8') as file:
         return json.load(file)
-    
+
 def get_city_config(city_name):
     """
     Retourne la configuration pour une ville donnée.
@@ -61,81 +75,25 @@ def fetch_events_from_api(api_url, limit):
         return response.json()['results']
     else:
         print(f"Erreur lors de la récupération des données : {response.status_code}")
-        return []  
-
-# Lecture des événements depuis le fichier CSV
-def read_events_from_csv():
-    """
-    Lit les événements existants depuis le fichier CSV.
-    """
-    events = {}
-    if os.path.exists(CSV_FILE):
-        with open(CSV_FILE, mode='r', newline='', encoding='utf-8') as file:
-            reader = csv.DictReader(file)
-            for row in reader:
-                # Utilise l'id_evenement_api comme clé
-                events[row['id_evenement_api']] = row
-    return events
-
-def write_events_to_csv(events):
-    """
-    Écrit les événements dans le fichier CSV.
-    """
-    with open(CSV_FILE, mode='w', newline='', encoding='utf-8') as file:
-        fieldnames = [
-            'id_evenement_api', 'ville_id', 'nom', 'description', 
-            'date_debut', 'date_fin', 'latitude', 'longitude', 'adresse'
-        ]
-        writer = csv.DictWriter(file, fieldnames=fieldnames)
-        writer.writeheader()
-        for event in events.values():
-            writer.writerow(event)
-
+        return []
 
 def update_events(city_name):
     """
     Met à jour les événements pour une ville donnée en utilisant les données de l'API.
     """
-
     city_config = get_city_config(city_name)
 
+    if not city_config:
+        return
+
     api_url = city_config['api_url']
-    ville_id = city_config['ville_id']
     config = load_config()
-    limit = config.get('limit', 100) # Récupère la limit (100 par défault)
-    
-    existing_events = read_events_from_csv()
+    limit = config.get('limit', 100)  # Récupère la limite (100 par défaut)
+
     new_events = fetch_events_from_api(api_url, limit)
 
-    for event in new_events:
-
-        id_evenement_api = event.get('identifiant')
-        nom = event.get('nom_de_la_manifestation')
-
-        # Extraire les champs nécessaires
-        event_data = {
-            'id_evenement_api': id_evenement_api,
-            'ville_id': ville_id,
-            'nom': nom,
-            'description': event.get('descriptif_court', '')[:255],
-            'date_debut': event.get('date_debut', ''),
-            'date_fin': event.get('date_fin', ''),
-            'latitude': event.get('googlemap_latitude', ''),
-            'longitude': event.get('googlemap_longitude', ''),
-            'adresse': event.get('lieu_adresse_2', 'Adresse inconnue')
-        }
-
-        if id_evenement_api in existing_events:
-            # Comparer les champs pour voir s'il y a eu des modifications
-            if existing_events[id_evenement_api] != event_data:
-                print(f"Mise à jour de l'événement : {nom}")
-                existing_events[id_evenement_api] = event_data
-        else:
-            print(f"Ajout d'un nouvel événement : {nom}")
-            existing_events[id_evenement_api] = event_data
-
-    # Sauvegarder les événements mis à jour dans le CSV
-    write_events_to_csv(existing_events)
+    # Sauvegarde les nouveaux événements dans la base de données
+    save_to_mongodb(new_events, uri, dbName, collectionName)
 
 if __name__ == "__main__":
     city_name = read_arguments()
