37b0b85f512e8a6fa76e87dfe26e41708375e385
ZYJ <yongjiazeng@yongjias-MacBook-Air.local>
2025-01-10 14:33:30 +0100

    merge csv

--- connectDB.py
+++ connectDB.py
@@ -1,5 +1,6 @@
 import csv
 import os
+import pandas as pd
 from pymongo import MongoClient
 from config import (
     MONGO_URI_Theo,
@@ -46,6 +47,27 @@ def export_filtered_data(filtered_data, output_csv_name):
     except Exception as e:
         print(f"Erreur lors de l'export des données : {e}")
 
+def merger_two_csv(csv1_name,csv2_name):
+    
+    csv1_path = os.path.join("filtered_data", csv1_name)
+    csv2_path = os.path.join("filtered_data", csv2_name)
+    df1 = pd.read_csv(csv1_path)
+    df2 = pd.read_csv(csv2_path)
+
+    df1['timestamp'] = pd.to_datetime(df1['timestamp'])
+    df2['timestamp'] = pd.to_datetime(df2['timestamp'])
+
+    # Fusionner les deux DataFrames sur le champ 'timestamp'
+    merged_df = pd.merge(df1, df2, on='timestamp', how='inner')  # 'inner' pour conserver uniquement les lignes correspondantes
+
+    # Exporter le résultat dans un nouveau fichier CSV
+    csv1_base = os.path.splitext(csv1_name)[0]
+    csv2_base = os.path.splitext(csv2_name)[0]
+    output_name = f"merge_{csv1_base}_{csv2_base}.csv"
+    output_path = os.path.join("filtered_data", output_name)
+    merged_df.to_csv(output_path, index=False)
+
+    print(f"Fichier fusionné exporté avec succès : {output_path}")
 
 
 def main():
@@ -65,6 +87,9 @@ def main():
         station_number=44
         file_name = f"bike_{station_number}.csv"
         export_filtered_data(filter_one_bike_data(collectionBikes,station_number),file_name)
+    
+    merger_two_csv("bike_44.csv","weather_data_filtered.csv")
+
     
     '''
         export_filtered_data(bike_position_data(collectionBikes), "bikes_position.csv")
