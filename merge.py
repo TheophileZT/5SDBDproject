3395eb2ab129a6de6f737bc4602615c224d06ba4
ZYJ <yongjiazeng@yongjias-MacBook-Air.local>
2025-01-14 10:36:21 +0100

    corrige le soucis de merge

--- merge.py
+++ merge.py
@@ -25,6 +25,8 @@ def merger_two_csv(csv1_name,csv2_name):
 
     print(f"Fichier fusionné exporté avec succès : {output_path}")
 
+
+##il y a soucis de merger
 def merger_three_csv(csv1_name,csv2_name,csv3_name):
     
     csv1_path = os.path.join("filtered_data", csv1_name)
@@ -36,11 +38,16 @@ def merger_three_csv(csv1_name,csv2_name,csv3_name):
 
     df1['timestamp'] = pd.to_datetime(df1['timestamp'])
     df2['timestamp'] = pd.to_datetime(df2['timestamp'])
-    df3['timestamp'] = pd.to_datetime(df2['timestamp'])
+    df3['timestamp'] = pd.to_datetime(df3['timestamp'])
 
     # Fusionner les deux DataFrames sur le champ 'timestamp'
     merged_df = pd.merge(df1, df2, on='timestamp', how='inner')
-    merged_df = pd.merge(merged_df, df3, on='timestamp', how='inner')
+    merged_df = pd.merge(merged_df, df3, on='timestamp', how='left')
+
+    # Les colonnes id_event et closest_stations resteront NaN si aucun événement ne correspond
+    merged_df['id_event'] = merged_df['id_event'].fillna('null')
+    merged_df['closest_stations'] = merged_df['closest_stations'].fillna('null')
+
 
     # Exporter le résultat dans un nouveau fichier CSV
     csv1_base = os.path.splitext(csv1_name)[0]
