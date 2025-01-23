dea4dfd8188e50c40a4aa16490be110c6938c4e9
yu Zhang <yuzhan2911@gmail.com>
2025-01-23 10:30:23 +0100

    add cluster

--- inference_one_station.py
+++ inference_one_station.py
@@ -4,27 +4,58 @@ from joblib import load
 from tensorflow.keras.models import load_model
 from tensorflow.keras.losses import MeanSquaredError
 from tensorflow.keras.metrics import MeanAbsoluteError, MeanSquaredError
+# obtenir le list 
+file_path = 'clustered_stations.csv'
+clustered_stations_data = pd.read_csv(file_path)
+
+
+clustered_stations = clustered_stations_data.groupby('cluster')['station'].apply(list).to_dict()
+
 
 # Charger le scaler et le modèle
-def load_model_scaler() :
-    scaler_x = load("scaler_X_one_station.pkl")
-    model = load_model(
+def load_all_model_scaler() :
+    scalers_x = {0:load("scaler_X_one_station.pkl"),
+                 1:load("scaler_X_one_station.pkl"),
+                 2:load("scaler_X_one_station.pkl"),
+                 3:load("scaler_X_one_station.pkl"),}
+    models = {0: load_model(
         "cnn_model_for_one_station.h5",
         custom_objects={
             "mse": MeanSquaredError(),
-            "mae": MeanAbsoluteError()
-        }
-    )
-    return scaler_x,model
+            "mae": MeanAbsoluteError()}),
+            1: load_model(
+        "cnn_model_for_one_station.h5",
+        custom_objects={
+            "mse": MeanSquaredError(),
+            "mae": MeanAbsoluteError()}),
+            2:load_model(
+        "cnn_model_for_one_station.h5",
+        custom_objects={
+            "mse": MeanSquaredError(),
+            "mae": MeanAbsoluteError()}),
+            3:load_model(
+        "cnn_model_for_one_station.h5",
+        custom_objects={
+            "mse": MeanSquaredError(),
+            "mae": MeanAbsoluteError()})
+    }
+    return scalers_x,models
 
+def get_cluster(number):
+    for cluster, stations in clustered_stations.items():
+        if number in stations:
+            return cluster
+    raise ValueError(f"Station number {number} does not belong to any cluster.")
 
 def predict(data):
-    scaler_x,model=load_model_scaler()
+    scalers_x,models=load_all_model_scaler()
     data=list_to_dataframe(data)
     # traiter les donnes commes dans le l'entrainement de model  
     data['timestamp'] = pd.to_datetime(data['timestamp'])
     data['timestamp_numeric'] = data['timestamp'].astype('int64') // 10**9
     data['day_of_week'] = data['timestamp'].dt.dayofweek
+    data['cluster'] = data['number'].apply(get_cluster)
+
     data = data.drop(columns=['timestamp', 'is_rainy'])  
 
     # Appliquer la standardisation sur 8 colones
@@ -33,11 +64,16 @@ def predict(data):
         'feels_like_temperature', 'wind_speed', 'counter_events',
         'timestamp_numeric', 'day_of_week'
     ]
-    data[standardScale_feature] = scaler_x.transform(data[standardScale_feature])
-    data_reshaped = data.to_numpy().reshape((1, 1, data.shape[1]))
-
-    # Effectuer la prédiction
-    predictions = np.round(model.predict(data_reshaped)).astype(int)[0, 0]
+    predictions = []
+    grouped = data.groupby('cluster')
+    for cluster, group in grouped:
+        scaler_x = scalers_x[cluster]
+        model = models[cluster]
+        group[standardScale_feature] = scaler_x.transform(group[standardScale_feature])
+        group_reshaped = group[standardScale_feature].to_numpy().reshape((group.shape[0], 1, len(standardScale_feature)))
+        cluster_predictions = np.round(model.predict(group_reshaped)).astype(int).flatten()
+        for number, prediction in zip(group['number'], cluster_predictions):
+            predictions.append({'number': number, 'prediction': prediction})
     print("Prédictions :", predictions)
     return predictions
 
