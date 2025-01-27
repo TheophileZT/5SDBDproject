ac2cb4f38e6e89f6b680e8a46beddf2923560772
tztlaptop <theozt25@gmail.com>
2025-01-27 18:21:46 +0100

    updated footer

--- Frontend/components/Footer/Footer.js
+++ Frontend/components/Footer/Footer.js
@@ -7,11 +7,10 @@ const Footer = ({ ...rest }) => {
     <footer className={styles.footer} {...rest}>
       <Container className={`${styles.footerContainer} ${styles.footerLegal}`}>
         <p>
-          &copy; <a href="https://spacejelly.dev">5SDBD INSA Toulouse</a>, {new Date().getFullYear()}
+          &copy; <a href="https://github.com/TheophileZT/5SDBDproject">5SDBD INSA Toulouse</a>, {new Date().getFullYear()}
         </p>
       </Container>
     </footer>
   );
 };
-// TODO : Mettre le lien du github ici merci
 export default Footer;
